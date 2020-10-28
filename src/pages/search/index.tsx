b52a966e768b413fd5445614395be7ea74a1d655
Mateus Cordeiro <mateusmcordeiro@hotmail.com>
2020-10-27 22:36:39 -0300

    input/button effects and triggeres

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../shared/components/input';
 import './App.scss';
 
+
+
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [organizationName, setOrganizationName] = useState<string>('');
+
   return (
     <div className="App">
       <header className="App-header">
         <Input
-          onChange={(e) => {console.log(e)}}
-          loading={true}
+          onChangeInput={
+            (e) => {
+              setOrganizationName(e.target.value)
+            }
+          }
+          onClickButton={
+            (e) => {
+              setLoading(true);
+            }
+          }
+          loading={loading}
+          selfValue={organizationName}
         ></Input>
       </header>
     </div>
--- src/shared/components/input/index.tsx
+++ src/shared/components/input/index.tsx
@@ -2,15 +2,32 @@ import React from 'react';
 import classNames from '../../helpers/classNames';
 import Spinner from '../spinner';
 import './input.scss';
+import { FiChevronRight } from "react-icons/fi";
 
 interface InputProps {
   loading: boolean,
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onChangeInput?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  onClickButton?: (e: React.MouseEvent<HTMLButtonElement>) => void,
+  selfValue?: string
 }
 
-function Input({ loading, onChange }: InputProps) {
+const localInputRef = React.createRef<HTMLInputElement>();
+
+function Input({ loading, onChangeInput = () => {}, onClickButton = () => {}, selfValue = '' }: InputProps) {
   return (
-    <span className="inputGroup">
+    <div className='inputGroup'>
+      <button 
+        className={
+          classNames({
+            inputGroup__button: true,
+            'inputGroup__button--active': selfValue.length > 0
+          })
+        }
+        onClick={onClickButton}
+      
+      >
+          <FiChevronRight size={30}></FiChevronRight>
+      </button>
       <input 
         className={
           classNames({
@@ -18,16 +35,17 @@ function Input({ loading, onChange }: InputProps) {
             'inputGroup__input--disabled': loading
           })
         }
-        onChange={ onChange }
-        id="myInput"
-        type="text"
-        placeholder="Ex: Facebook"
+        ref={ localInputRef }
+        onChange={ onChangeInput }
+        id='myInput'
+        type='text'
+        placeholder='Ex: Facebook'
       />
-      <label className="inputGroup__label" htmlFor="myInput">
-        { loading ? 'teste' : 'Organization Name' }
+      <label className='inputGroup__label' htmlFor='myInput'>
+        { loading ? localInputRef.current?.value : 'Organization Name' }
         <Spinner active={loading}></Spinner>
       </label>
-    </span>
+    </div>
   );
 }
 
