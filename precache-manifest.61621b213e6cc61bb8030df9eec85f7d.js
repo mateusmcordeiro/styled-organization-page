6cef8f770311a16aa7263b4c8b90698acbc01172
Mateus Cordeiro <mateusmcordeiro@hotmail.com>
2020-10-29 00:39:51 -0300

    Updates

---
+++ precache-manifest.61621b213e6cc61bb8030df9eec85f7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b5a42aa92e87b8e60737e4e350e1af",
+    "url": "/styled-organization-page/index.html"
+  },
+  {
+    "revision": "5fc5902931aa972a8494",
+    "url": "/styled-organization-page/static/css/2.c677075e.chunk.css"
+  },
+  {
+    "revision": "b0f9e370017d84ca1bfa",
+    "url": "/styled-organization-page/static/css/main.900120c6.chunk.css"
+  },
+  {
+    "revision": "5fc5902931aa972a8494",
+    "url": "/styled-organization-page/static/js/2.0fb1d1b2.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/styled-organization-page/static/js/2.0fb1d1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f9e370017d84ca1bfa",
+    "url": "/styled-organization-page/static/js/main.4ebe8040.chunk.js"
+  },
+  {
+    "revision": "d976807e4fcd31ce3ddd",
+    "url": "/styled-organization-page/static/js/runtime-main.4b541757.js"
+  }
+]);
